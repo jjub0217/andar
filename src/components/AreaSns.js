83823a7eb87fc869ebde9a8dcde86ed0210d7c8f
jjub0217 <noonssub1004@gmail.com>
2024-11-13 14:51:37 +0900

    배포 준비

---
+++ src/components/AreaSns.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AreaSns = () => {
+  const snsLinks = [
+    { href: "", label: "페이스북", className: "link_facebook" },
+    { href: "", label: "유튜브", className: "link_youtube" },
+    { href: "", label: "인스타그램", className: "link_instagram" },
+  ];
+
+
+  return (
+    <div className="sns_area">
+      <ul className="sns_list">
+        {snsLinks.map((link, index) => (
+          <li key={index}>
+            <a href={link.href} className={link.className}>
+              <span className="blind">{link.label}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AreaSns;
---
+++ src/components/MarkList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const MarkList = () => {
+
+  const marks = [
+    {
+      href: "",
+      label: "공정거래위원회",
+      className: "link_mark certification",
+    },
+    { href: "", label: "symantec", className: "link_mark symantec" },
+    { href: "", label: "kg", className: "link_mark kg" },
+  ];
+
+  return (
+    <ul className="mark_list">
+      {marks.map((mark, index) => (
+        <li className="mark_item" key={index}>
+          <a href={mark.href} className={mark.className}>
+            <span className="blind">{mark.label}</span>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MarkList;
